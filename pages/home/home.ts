1b342205ead87f1395a105cd46d1885c0943c2b0
josephaddad1 <josephaddad1@gmail.com>
2021-02-22 00:17:03 +0200

    alert for navigating to another page, modify the answer form to adjust my  data

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,71 +1,80 @@
-import { Component } from '@angular/core';
-import { NavController , AlertController } from 'ionic-angular';
-import { TestPage } from '../test/test'
+import { Component } from "@angular/core";
+import { NavController, AlertController } from "ionic-angular";
+import { TestPage } from "../test/test";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
-
-
 export class HomePage {
-  user:userData ={
-    name:'',
-    email:'',
-    testType:''
+  user: userData = {
+    name: "",
+    email: "",
+    testType: ""
   };
-  listOfTest = [{
-    id:1,
-    code:'ng',
-    name:'Angular'
-  },
-  {
-    id:2,
-    code:'js',
-    name:'JavaScript'
-  },
-  {
-    id:3,
-    code:'css',
-    name:'CSS Stlying'
-  },
-  {
-    id:4,
-    code:'ctet',
-    name:'CTET'
-  }
-  ]
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
-
-  }
+  listOfTest = [
+    {
+      id: 1,
+      code: "ng",
+      name: "Angular"
+    },
+    {
+      id: 2,
+      code: "js",
+      name: "JavaScript"
+    },
+    {
+      id: 3,
+      code: "css",
+      name: "CSS Stlying"
+    },
+    {
+      id: 4,
+      code: "ctet",
+      name: "CTET"
+    }
+  ];
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController
+  ) {}
 
   submitCheck() {
-    if(this.user.name && this.user.email && this.user.testType){
-      
-      this.nextPage();
-    } else{
-      const alert = this.alertCtrl.create({
-      title: 'Input required',
-      subTitle: 'Please provide the data, fill the all fields ',
-      buttons: ['OK']
+    const alert = this.alertCtrl.create({
+      title: "Start Exam",
+      subTitle: "Do You Want To Star The Exam?",
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: blah => {
+            console.log("Confirm Cancel: blah");
+          }
+        },
+        {
+          text: "Yes",
+          handler: () => {
+            this.nextPage();
+
+            console.log("Confirm Okay");
+          }
+        }
+      ]
     });
     alert.present();
-    }
-    
   }
 
-nextPage()
-{
-  console.log(this.user);
-this.navCtrl.push(TestPage,{
+  nextPage() {
+    console.log(this.user);
+    this.navCtrl.push(TestPage, {
       data: this.user
     });
-}
-
+  }
 }
 
 interface userData {
-  name:string,
-  email:string,
-  testType:string
+  name: string;
+  email: string;
+  testType: string;
 }
--- pages/test/test.ts
+++ pages/test/test.ts
@@ -1,23 +1,21 @@
-import { Component } from '@angular/core';
-import { NavController , NavParams} from 'ionic-angular';
-import {ResultPage} from '../result/result';
-import { SharedDataService } from '../../shared/shared-data-service'
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+import { ResultPage } from "../result/result";
+import { SharedDataService } from "../../shared/shared-data-service";
 
-
-  /**
-   *@author acharyaks90
-   *@description test Page Copponent
-   *@Component Class
-   */
-
+/**
+ *@author acharyaks90
+ *@description test Page Copponent
+ *@Component Class
+ */
 
 @Component({
-  selector: 'page-test',
-  templateUrl: 'test.html'
+  selector: "page-test",
+  templateUrl: "test.html"
 })
 export class TestPage {
   test: string = "NG test";
-  relationship = '';
+  relationship = "";
   t1 = new Date(0);
   timerInterval: any;
   timer = 0;
@@ -25,131 +23,148 @@ export class TestPage {
   user = {};
   currentQuestion = 1;
   questionList = [];
-  ques = { descr : "What is Angular Js",
-           quesNo: 1 ,
-           options:[
-             {
-               descr:"Framework",
-               seq:"12"
-             },
-              {
-               descr:"Library",
-               seq:"11"
-             },
-              {
-               descr:"Browser",
-               seq:"10"
-             },
-              {
-               descr:"Language",
-               seq:"9"
-             },
-             
-           ],
-           answer: 12,
-           answerFilled:''
-  }
+  ques = {};
+  private exam = {
+    id: "207f4ce7-70d4-42c4-9b64-c9de0fd60708",
+    name: "EXAM 1",
+    note: 22,
+    duration: 60,
+    time: "1970-01-01T10:54:01.000+00:00",
+    date: "2021-01-29T22:00:00.000+00:00",
+    questionList: [
+      {
+        id: "4765bae6-f145-4df8-838d-45127dbcbeef",
+        name: "Q1 radio",
+        note: 23,
+        type: "radio",
+        textAnswer: null,
+        answerList: [
+          {
+            id: "65c0fd4d-736b-4fdd-977f-d5c2fe58538a",
+            name: "test asd `23",
+            correct: false
+          },
+          {
+            id: "7ec79190-c81a-4799-9ce7-873e8ebeb8bf",
+            name: "4redasd sadad",
+            correct: false
+          }
+        ]
+      },
+      {
+        id: "6b88198b-8721-48d9-8865-45e1f5b8d0fd",
+        name: "QUEST 1",
+        note: 2,
+        type: "text",
+        textAnswer: "Q1",
+        answerList: []
+      }
+    ]
+  };
+  private examFormDTO = {
+    service: {},
+    text: {},
+    textarea: {},
+    number: {},
+    select: {},
+    radio: {},
+    checkbox: {}
+  };
 
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public sharedDataService: SharedDataService
+  ) {
+    // this.user = this.navParams.get('data');
+    this.questionList = this.exam.questionList;
+    this.questionList.map(item => {
+      item.answerFilled = "";
+    });
+    this.loadQuestion();
+    this.t1.setHours(0, 20, 0, 0);
+    this.timer = this.t1.getTime();
+    this.timerInterval = setInterval(() => {
+      this.timer = this.timer - 1000;
+    }, 1000);
+
+    setTimeout(() => {
+      this.completeTest();
+    }, 720000);
+  }
   /**
- * @description Initialize function
- * @constructor
- * @param {object} NavController - The title of the book.
- * @param {object} NavParams - The author of the book.
- */
-  constructor( public navCtrl: NavController,
-               public navParams: NavParams,
-               public sharedDataService: SharedDataService ) {
-                this.user = this.navParams.get('data');
-                this.questionList = sharedDataService.getExamQuestions();
-                this.questionList.map((item)=>{
-                  item.answerFilled='';
-                })
-                this.loadQuestion();
-                this.t1.setHours( 0,20,0,0);
-                this.timer = this.t1.getTime() ;
-                this.timerInterval =setInterval(()=>{
-                  this.timer = this.timer  -1000;
-                },1000)
+   * @author acharyaks90
+   * @description Finish the test
+   * @method
+   */
 
-                setTimeout(()=>{
-                     this.completeTest(); 
-                },720000)
-  
-  }
-   /**
-    * @author acharyaks90
-    * @description Finish the test
-    * @method
-    */
-  completeTest(){
+  completeTest() {
     this.selectedAnswer();
-    this.navCtrl.push(ResultPage,{
+    this.navCtrl.push(ResultPage, {
       data: {
-        user:this.user,
-        test:{seq:12,
-             score:this.score}
+        user: this.user,
+        test: { seq: 12, score: this.score }
       }
     });
   }
 
   /**
-    * @author acharyaks90
-    * @description Load Questions from test DB
-    * @method loadQuestion
-    */
+   * @author acharyaks90
+   * @description Load Questions from test DB
+   * @method loadQuestion
+   */
 
-  loadQuestion(){
-    this.ques = this.questionList[this.currentQuestion-1];
+  loadQuestion() {
+    this.ques = this.questionList[this.currentQuestion - 1];
   }
 
-    /**
-    * @author acharyaks90
-    * Load Previous Question
-    * @method previousQuestion
-    */
+  /**
+   * @author acharyaks90
+   * Load Previous Question
+   * @method previousQuestion
+   */
 
-  previousQuestion(){
-    if(this.currentQuestion > 1){
+  previousQuestion() {
+    if (this.currentQuestion > 1) {
       --this.currentQuestion;
-      this.loadQuestion()
+      this.loadQuestion();
     }
   }
 
   /**
-    * @author acharyaks90
-    * @description Load Next Question
-    * @method nextQuestion
-    */
-   nextQuestion(){
-    if(this.currentQuestion < this.questionList.length){
+   * @author acharyaks90
+   * @description Load Next Question
+   * @method nextQuestion
+   */
+  nextQuestion() {
+    if (this.currentQuestion < this.questionList.length) {
       ++this.currentQuestion;
-      this.loadQuestion()
+      this.loadQuestion();
     }
   }
 
   /**
-    * @author acharyaks90
-    * @description Event fired on answering the question
-    * @method nextQuestion
-    */
-  
-  selectedAnswer(){
-        this.score =0;
-      this.questionList.forEach((item)=>{
-        if(item.answerFilled && item.answer == item.answerFilled ){
-          this.score = this.score + 5;
-        }
-      })
+   * @author acharyaks90
+   * @description Event fired on answering the question
+   * @method nextQuestion
+   */
+
+  selectedAnswer() {
+    this.score = 0;
+    this.questionList.forEach(item => {
+      if (item.answerFilled && item.answer == item.answerFilled) {
+        this.score = this.score + 5;
+      }
+    });
   }
 
-   /**
-    * @author acharyaks90
-    * @description Event fired on answering the question
-    * @method ngOnDestroy
-    */
+  /**
+   * @author acharyaks90
+   * @description Event fired on answering the question
+   * @method ngOnDestroy
+   */
 
   ngOnDestroy() {
-     window.clearInterval(this.timerInterval);
+    window.clearInterval(this.timerInterval);
   }
-
 }
